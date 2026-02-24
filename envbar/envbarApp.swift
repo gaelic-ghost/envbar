09af964951667ef168b586565376cd59595cba45
Gale W <mail@galewilliams.com>
2026-02-24 16:31:47 -0500

    Initial Commit

---
+++ envbar/envbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  envbarApp.swift
+//  envbar
+//
+//  Created by Gale Williams on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct envbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
